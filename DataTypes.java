50cb950ea5719e48fe65398ef51b582f601b5080
reddyshruth <161217347+reddyshruth@users.noreply.github.com>
2026-01-05 11:40:05 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class DataTypes{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int age=21;
+    float marks=3.14f;
+    double salary=25000.00;
+    char grade='A';
+    boolean password=true;
+    String name="Shruthi";
+    System.out.println("Age: "+ age);
+    System.out.println("Marks: "+ marks);
+    System.out.println("Salary: "+ salary);
+    System.out.println("Grade: "+ grade);
+    System.out.println("Password: "+ password);
+    System.out.println("Name: "+ name);
+}
+}
+
